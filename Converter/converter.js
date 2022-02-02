3c7990bb03f5471007ad03a6044cd069ad72860c
PhilosopherProgrammer <nekarakube@gmail.com>
2022-02-02 15:34:58 +0900

    Change path

--- Converter/converter.js
+++ Converter/converter.js
@@ -1,36 +1,36 @@
 const fs = require('fs');
-const {solvedProblemModel} = require('../Model/SolvedProblem.model');
+const {codingTestModel} = require('../Model/CodingTest.model');
 
-const convert = Object.keys(solvedProblemModel).map((key) => {        
-    const num_name = solvedProblemModel[key].name.split(" "); 
+const convert = Object.keys(codingTestModel).map((key) => {        
+    const num_name = codingTestModel[key].name.split(" "); 
     let name, number;
     if (!isNaN(num_name[0])) {
         number = parseInt(num_name[0]);
         name = num_name.slice(1).join(" ");
     } else {
-        name = solvedProblemModel[key].name
+        name = codingTestModel[key].name
     }
 
     return number !== undefined ? {
         cote_id: key,
         name: name,
         number: number,
-        platform: solvedProblemModel[key].platform,
-        difficulty: solvedProblemModel[key].difficulty,
-        language: solvedProblemModel[key].language,
-        topic: solvedProblemModel[key].topic,
-        url: solvedProblemModel[key].url
+        platform: codingTestModel[key].platform,
+        difficulty: codingTestModel[key].difficulty,
+        language: codingTestModel[key].language,
+        topic: codingTestModel[key].topic,
+        url: codingTestModel[key].url
     } : {
         cote_id: key,
         name: name,
-        platform: solvedProblemModel[key].platform,
-        difficulty: solvedProblemModel[key].difficulty,
-        language: solvedProblemModel[key].language,
-        topic: solvedProblemModel[key].topic,
-        url: solvedProblemModel[key].url
+        platform: codingTestModel[key].platform,
+        difficulty: codingTestModel[key].difficulty,
+        language: codingTestModel[key].language,
+        topic: codingTestModel[key].topic,
+        url: codingTestModel[key].url
     }
 });
 
 fs.writeFileSync('convert.json', JSON.stringify(convert));
 
-module.exports = { solvedProblemModel };
+module.exports = { codingTestModel };
