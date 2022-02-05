3f6e76cae08f66976295930835c61ebff08c3803
PhilosopherProgrammer <nekarakube@gmail.com>
2022-02-05 20:47:51 +0900

    challenge model init

---
+++ src/model/Challenge.model.ts
@@ -0,0 +1,11 @@
+export enum ChallengeName {
+    /** Frontend Mentor */
+    NFT_PREVIEW_CARD_COMPONENT = "NFT Preview Card Component",
+    TIME_TRACKING_DASHBOARD = "Time Tracking Dashboard"
+}
+
+export enum ChallengeId {
+    /** Frontend Mentor */
+    NFT_PREVIEW_CARD_COMPONENT = "_5l7ujx3",
+    TIME_TRACKING_DASHBOARD = "_tfzfvc3"
+}
