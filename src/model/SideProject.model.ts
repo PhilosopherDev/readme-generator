0681f9c7c6edbe6a02bce013e244bb6809b528fa
PhilosopherProgrammer <nekarakube@gmail.com>
2022-02-05 20:48:17 +0900

    Side project moel init

---
+++ src/model/SideProject.model.ts
@@ -0,0 +1,11 @@
+export enum SideProjectName {
+    BYUNMIN_CHOI = "byungmin-choi.com",
+    README_GENERATOR = "Readme Generator",
+    SENTENCE_SHAKER = "Sentence Shaker"
+}
+
+export enum SideProjectId {
+    BYUNMIN_CHOI = "_zzhfhcb",
+    README_GENERATOR = "_jvdshfp",
+    SENTENCE_SHAKER = "_n1ah3tt"
+}
