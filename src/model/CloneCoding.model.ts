9265ba92a03e4043423f183b517c6604185fe95e
PhilosopherProgrammer <nekarakube@gmail.com>
2022-02-05 20:48:00 +0900

    Clone coding model init

---
+++ src/model/CloneCoding.model.ts
@@ -0,0 +1,16 @@
+export enum CloneCodingName {
+    /** Brad Traversy */
+    HOTEL_BT = "HotelBT",
+
+    /** Interactive Developer */
+    SWAYING_BOX_HANGING_FROM_A_STRING = "줄에 매달려 흔들리는 상자"
+
+}
+
+export enum CloneCodingId {
+    /** Brad Traversy */
+    HOTEL_BT = "_j61nfpd",
+
+    /** Interactive Developer */
+    SWAYING_BOX_HANGING_FROM_A_STRING = "_13mg8n4"
+}
