f0e1992f07a7399dd411dc201eb5e8fa19028717
PhilosopherProgrammer <nekarakube@gmail.com>
2022-03-05 12:19:34 +0900

    Add data

--- src/data/CodingTest.data.ts
+++ src/data/CodingTest.data.ts
@@ -2927,7 +2927,7 @@ export const codingTestData: Array<CodingTestModel> = [
           "link": "https://github.com/PhilosopherProgrammer/Coding-Test-Practice/tree/main/CodingTestPlatform/Programmers/Level1/%EB%AC%B8%EC%9E%90%EC%97%B4%EB%82%B4p%EC%99%80y%EC%9D%98%EA%B0%9C%EC%88%98"
       }]
     },
-  {
+    {
       "cote_id": "_t2w2df9",
       "name": "숫자의 표현",
       "platform": CodingTestPlatform.PROGRAMMERS,
@@ -2943,7 +2943,24 @@ export const codingTestData: Array<CodingTestModel> = [
         "icon": URLIcon.CODE,
         "link": "https://github.com/PhilosopherProgrammer/Coding-Test-Practice/tree/main/CodingTestPlatform/Programmers/Level2/%EC%88%AB%EC%9E%90%EC%9D%98%ED%91%9C%ED%98%84"
       }]
-  },
+    },
+    { 
+      "cote_id": "_r9cyse2",
+      "name": "영어 끝말잇기",
+      "platform": CodingTestPlatform.PROGRAMMERS,
+      "difficulty": ProgrammersLevel.LEVEL2,
+      "language": [Language.JAVASCRIPT],
+      "topic": [DataStructure.ARRAY],
+      "url": [{
+          "name": URLName.BLOG,
+          "icon": URLIcon.BLOG,
+          "link": "https://philosopherprogrammer.com/107"
+      }, {
+          "name": URLName.CODE,
+          "icon": URLIcon.CODE,
+          "link": "https://github.com/PhilosopherProgrammer/Coding-Test-Practice/tree/main/CodingTestPlatform/Programmers/Level2/%EC%98%81%EC%96%B4%EB%81%9D%EB%A7%90%EC%9E%87%EA%B8%B0"
+      }]
+    },
 ]
 
 /**
